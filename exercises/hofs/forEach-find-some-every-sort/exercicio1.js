f55d462f6817ff88469f2169c419c2d436a59745
lucas <mr.pine97@gmail.com>
2021-07-29 23:06:24 -0300

    adiciona a função authorBornIn1947

--- exercises/hofs/forEach-find-some-every-sort/exercicio1.js
+++ exercises/hofs/forEach-find-some-every-sort/exercicio1.js
@@ -1,3 +1,17 @@
+const assert = require('assert');
 let {books} = require('./database')
 
-console.log(books[0])
+//1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+
+// console.log(books.find((element) => element.author.birthYear === 1947))
+
+// const findAuthorByBirthYear = (year) => (books.find((book) => book.author.birthYear === year)).author.name;
+// console.log(findAuthorByBirthYear(1947));
+
+///////////////////////////////////////////////////////////
+
+// console.log(books.find((book) => (book.author.birthYear === 1947)).author.name)
+
+const authorBornIn1947 = () => books.find((book) => book.author.birthYear === 1947).author.name
+
+assert.strictEqual(authorBornIn1947(), 'Stephen King');
