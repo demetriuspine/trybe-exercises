035ffd296acf70417fb6091c9e5db5263b5c0b7b
lucas <mr.pine97@gmail.com>
2021-07-29 23:21:15 -0300

    adiciona a função smallerName

---
+++ exercises/hofs/forEach-find-some-every-sort/exercicio2.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+let {books} = require('./database')
+
+//Retorne o nome do livro de menor nome.
+
+function smallerName() {
+  let nameBook;
+  // escreva aqui o seu código
+  nameBook = books[0].name
+  books.forEach((book) => {
+    if (book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  })
+
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
+
+assert.strictEqual(smallerName(), 'Duna');
